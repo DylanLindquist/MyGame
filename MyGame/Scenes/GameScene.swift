66885ad8d8f1989f28ea619fd4e92712bbda9604
DylanLindquist <70230076+DylanLindquist@users.noreply.github.com>
2021-01-12 12:36:54 -0700

    did DropGameView code

---
+++ MyGame/Scenes/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  MyGame
+//
+//  Created by Lindquist, Dylan on 1/12/21.
+//
+
+import SpriteKit
+
+class GameScene : SKScene
+{
+    override func didMove(to view: SKView) -> Void
+    {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) -> Void
+    {
+        
+    }
+}
