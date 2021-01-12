3ddee8a4786711a28d90a26057f5935b22969c1e
DylanLindquist <70230076+DylanLindquist@users.noreply.github.com>
2021-01-12 12:19:02 -0700

    Ignore files

---
+++ MyGame/MyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGameApp.swift
+//  MyGame
+//
+//  Created by Lindquist, Dylan on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
